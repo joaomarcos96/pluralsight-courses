f11bd7ac8561cb9de4d47e0d5a65419ebc04d51d
joaomarcos96 <jao.markos@hotmail.com>
2020-04-08 21:07:01 -0300

    Initial commit

---
+++ Building a RESTful API with ASP.NET Core 3/CourseLibrary/CourseLibrary.API/Program.cs
@@ -0,0 +1,23 @@
+using CourseLibrary.API.DbContexts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace CourseLibrary.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
