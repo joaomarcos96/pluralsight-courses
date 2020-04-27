76ce34d154bfc434d419bd73041e2edd079f72a0
Joao Marcos <jao.markos@hotmail.com>
2020-04-27 20:24:41 -0300

    Add problem details logic

--- Building a RESTful API with ASP.NET Core 3/CourseLibrary/CourseLibrary.API/Startup.cs
+++ Building a RESTful API with ASP.NET Core 3/CourseLibrary/CourseLibrary.API/Startup.cs
@@ -5,11 +5,14 @@ using CourseLibrary.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CourseLibrary.API
 {
@@ -25,7 +28,51 @@ namespace CourseLibrary.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers(options => options.ReturnHttpNotAcceptable = true)
-                .AddXmlDataContractSerializerFormatters();
+                .AddXmlDataContractSerializerFormatters()
+                .ConfigureApiBehaviorOptions(setupAction =>
+                {
+                    setupAction.InvalidModelStateResponseFactory = context =>
+                    {
+                        var problemDetailsFactory = context.HttpContext.RequestServices
+                            .GetRequiredService<ProblemDetailsFactory>();
+
+                        var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                            context.HttpContext,
+                            context.ModelState);
+
+                        problemDetails.Detail = "See the errors field for details.";
+                        problemDetails.Instance = context.HttpContext.Request.Path;
+
+                        // find out which status code to use
+                        var actionExecutingContext = context as ActionExecutingContext;
+
+                        // if there are ModelState errors and all arguments were correctly
+                        // found/parsed, we're dealing with validation errors
+                        if (context.ModelState.ErrorCount > 0
+                            && actionExecutingContext?.ActionArguments.Count
+                            == context.ActionDescriptor.Parameters.Count)
+                        {
+                            problemDetails.Type = "https://pagecontainingtheerrortype.com";
+                            problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                            problemDetails.Title = "One or more validation errors occurred.";
+
+                            return new UnprocessableEntityObjectResult(problemDetails)
+                            {
+                                ContentTypes = { "application/problem+json" }
+                            };
+                        }
+
+                        // if one of the arguments wasn't correctly found / couldn't be parsed
+                        // we're dealing with null/unparseable input
+                        problemDetails.Status = StatusCodes.Status400BadRequest;
+                        problemDetails.Title = "One or more errors on input occurred.";
+
+                        return new BadRequestObjectResult(problemDetails)
+                        {
+                            ContentTypes = { "application/problem+json" }
+                        };
+                    };
+                });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
