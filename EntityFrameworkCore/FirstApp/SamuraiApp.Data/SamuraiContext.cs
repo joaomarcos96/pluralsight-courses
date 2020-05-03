88df0b911ab2b365396ceb3e1b18a6bcc83a0b75
Joao Marcos <jao.markos@hotmail.com>
2020-05-03 13:09:33 -0300

    Complete first module

---
+++ EntityFrameworkCore/FirstApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=samurai.db");
+        }
+    }
+}
